cbb44c0534278079275f701988c48239a9611c09
Ivan Pashkulev <ivan.pashkulev@gmail.com>
2018-09-07 21:05:54 +0300

    Added Owners Page and Controller. closes #13

---
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package guru.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping(value = {"", "/", "/index", "/index.html"}, method = RequestMethod.GET)
+    public String listOwners() {
+
+        return "owners/index";
+    }
+}
